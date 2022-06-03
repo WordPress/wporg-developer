bee877d76f38391dadba788de6fa76c0ed974e12
Dion Hulse <contact@dd32.id.au>
2022-06-03 16:09:12 +1000

    Just run all cron tasks, no need to manually call the specific handbooks..

--- env/import-handbooks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Import the handbooks. These are on cron events.
-for post_type in blocks wpcs rest-api; do
-	echo Importing the ${post_type} handbook...
-
-	wp cron event run devhub_${post_type}_import_manifest
-	wp cron event run devhub_${post_type}_import_all_markdown
-done
