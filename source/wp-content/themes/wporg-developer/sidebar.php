b920e3247897df65c8a9bed1fb4475faef91e61f
tellyworth <alex@automattic.com>
2022-06-03 15:47:27 +1000

    WIP: display a kinda-sorta sidebar menu on function reference pages
    There's a lot wrong with this, I hammered junk into place to make something work so we could see if it's worth doing properly.

--- source/wp-content/themes/wporg-developer/sidebar.php
+++ source/wp-content/themes/wporg-developer/sidebar.php
@@ -5,12 +5,14 @@
  * @package wporg-developer
  */
 ?>
+
 <?php if ( is_active_sidebar( get_post_type() ) ) : ?>
-	<div id="sidebar" class="widget-area sidebar section" role="complementary">
-		<?php do_action( 'before_sidebar' ); ?>
-
-		<?php if ( ! dynamic_sidebar( get_post_type() ) ) : ?>
-		<?php endif; // end sidebar widget area ?>
-
+	<div id="secondary" class="widget-area" role="complementary">
+		<a href="#" id="secondary-toggle"></a>
+		<div id="secondary-content">
+			<?php function_reference_sidebar_menu(); ?>
+			<?php do_action( 'before_sidebar' ); ?>
+			<?php dynamic_sidebar( get_query_var( 'current_handbook' ) ); ?>
+		</div>
 	</div><!-- #secondary -->
 <?php endif; ?>
