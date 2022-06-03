9efefa059152ef7707d29040dcbc4c8cec5f15d0
Dion Hulse <contact@dd32.id.au>
2022-06-03 15:57:22 +1000

    Respect the local url in markdown imports.

--- env/0-sandbox.php
+++ env/0-sandbox.php
@@ -8,3 +8,14 @@ namespace WordPressdotorg\Developer;
 defined( 'WPINC' ) || die();
 
 require_once WPMU_PLUGIN_DIR . '/wporg-mu-plugins/mu-plugins/loader.php';
+
+/**
+ * Filter the imported content URI's to point to the local site, rather than developer.wordpress.org.
+ *
+ * @param string $html The imported & parsed markdown.
+ * @return string
+ */
+function wporg_markdown_after_transform( $html ) {
+	return str_replace( 'https://developer.wordpress.org/', home_url( '/' ), $html );
+}
+add_filter( 'wporg_markdown_after_transform', __NAMESPACE__ . '\wporg_markdown_after_transform', 100 );
