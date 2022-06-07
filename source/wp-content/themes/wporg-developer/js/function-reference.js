c5b44408b994f2aea4f5efb74b6caa2e924c46d6
tellyworth <alex@automattic.com>
2022-06-07 16:38:05 +1000

    Restore the code collapse & expand buttons

--- source/wp-content/themes/wporg-developer/js/function-reference.js
+++ source/wp-content/themes/wporg-developer/js/function-reference.js
@@ -7,6 +7,74 @@
 
 jQuery( function ( $ ) {
 	let $usesList, $usedByList, $showMoreUses, $hideMoreUses, $showMoreUsedBy, $hideMoreUsedBy;
+	let $sourceCollapsedHeight;
+
+	function onLoad() {
+		sourceCodeHighlightInit();
+
+		toggleUsageListInit();
+	}
+
+	function sourceCodeHighlightInit() {
+
+		// We require the SyntaxHighlighter javascript library
+		if ( undefined === window.Prism ) {
+			return;
+		}
+
+		// 1em (margin) + 10 * 17px + 10. Lines are 1.1em which rounds to 17px: calc( 1em + 17px * 10 + 10 ).
+		// Extra 10px added to partially show next line so it's clear there is more.
+		$sourceCollapsedHeight = 196;
+		sourceCodeDisplay();
+	}
+
+	function sourceCodeDisplay( element ) {
+
+		if ( element !== undefined ) {
+			// Find table inside a specific source code element if passed.
+			var sourceCode = $( '.source-content', element ).find( 'pre' );
+		} else {
+			// Find table inside all source code elements.
+			var sourceCode = $( '.source-content' ).find( 'pre' );
+		}
+
+		if ( !sourceCode.length ) {
+			return;
+		}
+
+		sourceCode.each( function( t ) {
+			if ( ( $sourceCollapsedHeight - 12 ) < $( this ).height() ) {
+
+				var sourceContent = $( this );//.closest( '.wp-block-code' );
+
+				// Do this with javascript so javascript-less can enjoy the total sourcecode
+				sourceContent.css( {
+					height: $sourceCollapsedHeight + 'px'
+				} );
+
+				sourceContent.next( '.source-code-links' ).find( 'span:first' ).show();
+				sourceContent.parent().find( '.show-complete-source' ).show();
+				sourceContent.parent().find( '.show-complete-source' ).off( 'click.togglesource' ).on( 'click.togglesource', toggleCompleteSource );
+				sourceContent.parent().find( '.less-complete-source' ).off( 'click.togglesource' ).on( 'click.togglesource', toggleCompleteSource );
+			}
+		} );
+	}
+
+	function toggleCompleteSource( e ) {
+		e.preventDefault();
+
+		var sourceContent = $( this ).closest( '.source-content' ).find( 'pre' );
+
+		if ( $( this ).parent().find( '.show-complete-source' ).is( ':visible' ) ) {
+			var heightGoal = sourceContent.find('code').height() + 45; // takes into consideration potential x-scrollbar
+		} else {
+			var heightGoal = $sourceCollapsedHeight;
+		}
+
+		sourceContent.animate( { height: heightGoal + 'px' } );
+
+		$( this ).parent().find( 'a' ).toggle();
+	}
 
 	function toggleUsageListInit() {
 		// We only expect one used_by and uses per document
@@ -84,4 +152,7 @@ jQuery( function ( $ ) {
 
 		$element.prepend( $button );
 	} );
+
+	$( onLoad );
+
 } );
