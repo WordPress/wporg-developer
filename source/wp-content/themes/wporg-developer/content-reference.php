43beffeeed3a62ba270b79dc5b8f55c1e1e759a0
Adam Wood <info@adamwood.co.nz>
2022-06-08 14:14:36 +1200

    Extract Table of Contents from content and fix it to the right - On medium screens position it within the header - On small screens hide it from view

--- source/wp-content/themes/wporg-developer/content-reference.php
+++ source/wp-content/themes/wporg-developer/content-reference.php
@@ -1,20 +1,7 @@
 <?php namespace DevHub; ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<?php if ( is_single() ) : 
 
-	<?php echo get_deprecated(); ?>
-
-	<?php echo get_private_access_message(); ?>
-
-	<h1><?php echo get_signature(); ?></h1>
-
-	<section class="summary">
-		<?php echo get_summary(); ?>
-	</section>
-
-<?php if ( is_single() ) : ?>
-
-	<?php
 	$content = get_reference_template_parts();
 
 	// If the Handbook TOC is available, use it.
@@ -22,14 +9,41 @@
 		$TOC = new \WPorg_Handbook_TOC( get_parsed_post_types(), array(
 			'header_text' => __( 'Contents', 'wporg' )
 		) );
+		
+		$TOC_parts = $TOC->add_toc( $content );
 
-		$content = '<div class="content-toc">' . $TOC->add_toc( $content ) . '</div>';
-
+		$content = $TOC_parts['content'];
+		$toc = $TOC_parts['toc'];
 	endif;
-	?>
 
-	<?php echo $content; ?>
+endif; ?>
 
-<?php endif; ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php echo get_deprecated(); ?>
+
+	<?php echo get_private_access_message(); ?>
+
+	<header class="post-header">
+		<div class="post-title">
+
+			<h1><?php echo get_signature(); ?></h1>
+			
+			<section class="summary">
+				<?php echo get_summary(); ?>
+			</section>
+		</div>
+		<?php if ( is_single() ) :
+
+			echo $toc;
+
+		endif; ?>
+	</header>
+
+<?php if ( is_single() ) :  
+
+	echo '<div class="content">' . $content . '</div>'; 
+
+endif; ?>
 
 </article>
