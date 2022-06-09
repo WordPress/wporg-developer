e8d162b245b3e26917cac2f290a48f0104fe6223
Adam Wood <adam.wood@automattic.com>
2022-06-10 11:37:34 +1200

    Revert doc change

--- source/wp-content/plugins/handbook/inc/table-of-contents.php
+++ source/wp-content/plugins/handbook/inc/table-of-contents.php
@@ -130,7 +130,7 @@ class WPorg_Handbook_TOC {
 	 * @access public
 	 *
 	 * @param string $content Content.
-	 * @return string toc => Modified Content.
+	 * @return string Modified content.
 	 */
 	public function add_toc( $content ) {
 		$parts = $this->parse_content( $content );
