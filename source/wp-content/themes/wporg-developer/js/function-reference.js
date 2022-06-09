522f8b41a6ee5b9c564901bb07e1b432d8d09bd2
ren <18050944+renintw@users.noreply.github.com>
2022-06-09 18:44:21 +0800

    Adjust MIN_HEIGHT to show correct amount of lines

--- source/wp-content/themes/wporg-developer/js/function-reference.js
+++ source/wp-content/themes/wporg-developer/js/function-reference.js
@@ -7,9 +7,9 @@
 
 // eslint-disable-next-line id-length -- $ OK.
 jQuery( function ( $ ) {
-	// 22.5px (line height) * 10 for 10 lines + 15px top padding + 10px extra.
+	// 22.5px (line height) * 10 for 10 lines + 15px top padding + 45px button height + 10px extra.
 	// The extra 10px added to partially show next line so it's clear there is more.
-	const MIN_HEIGHT = 22.5 * 10 + 15 + 10;
+	const MIN_HEIGHT = 22.5 * 10 + 15 + 45 + 10;
 
 	function collapseCodeBlock( $element, $button ) {
 		$button.text( wporgFunctionReferenceI18n.expand );
