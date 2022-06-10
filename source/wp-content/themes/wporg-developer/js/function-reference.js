d279399b838ecb36e446e789111f01b150845e15
Kelly Dwan <ryelle@users.noreply.github.com>
2022-06-10 12:46:57 -0400

    Code Blocks: Inject opening PHP tag via Prism hooks before parsing the syntax. (#111)
    * Tools: Update composer packages
    
    * Code Blocks: Inject opening PHP tag via Prism hooks before parsing the syntax.
    
    * Check for just `<`, to prevent added the tag on HTML code

--- source/wp-content/themes/wporg-developer/js/function-reference.js
+++ source/wp-content/themes/wporg-developer/js/function-reference.js
@@ -1,4 +1,4 @@
-/* global jQuery, wporgFunctionReferenceI18n */
+/* global jQuery, Prism, wporgFunctionReferenceI18n */
 /**
  * function-reference.js
  *
@@ -85,7 +85,7 @@ jQuery( function ( $ ) {
 	let $usesList, $usedByList, $showMoreUses, $hideMoreUses, $showMoreUsedBy, $hideMoreUsedBy;
 
 	function toggleUsageListInit() {
-		var usesToShow   = $( '#uses-table' ).data( 'show' ),
+		var usesToShow = $( '#uses-table' ).data( 'show' ),
 			usedByToShow = $( '#used-by-table' ).data( 'show' );
 
 		// We only expect one used_by and uses per document
@@ -126,4 +126,25 @@ jQuery( function ( $ ) {
 	}
 
 	toggleUsageListInit();
+
+	// Runs before the highlight parsing is run.
+	// `env` is defined here: https://github.com/PrismJS/prism/blob/2815f699970eb8387d741e3ac886845ce5439afb/prism.js#L583-L588
+	Prism.hooks.add( 'before-highlight', function ( env ) {
+		// If the code starts with `<`, it's either already got an opening tag,
+		// or it starts with HTML. Either way, we don't want to inject here.
+		if ( 'php' === env.language && ! env.code.startsWith( '<' ) ) {
+			env.code = '<? ' + env.code;
+			env.hasAddedTag = true;
+		}
+	} );
+
+	// Runs before `highlightedCode` is set to the `innerHTML` of the container.
+	Prism.hooks.add( 'before-insert', function ( env ) {
+		if ( env.hasAddedTag ) {
+			env.highlightedCode = env.highlightedCode.replace(
+				'<span class="token delimiter important">&lt;?</span> ',
+				''
+			);
+		}
+	} );
 } );
