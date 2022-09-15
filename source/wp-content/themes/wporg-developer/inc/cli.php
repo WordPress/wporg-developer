109fa4046c212c9ffbd08439b45e92a9c2f0b400
haunzala <59374562+hz-tyfoon@users.noreply.github.com>
2022-09-15 21:43:57 +0600

    Update `wp-cli/handbook` branch to `main` (#141)

--- source/wp-content/themes/wporg-developer/inc/cli.php
+++ source/wp-content/themes/wporg-developer/inc/cli.php
@@ -2,7 +2,7 @@
 
 class DevHub_CLI {
 
-	private static $commands_manifest = 'https://raw.githubusercontent.com/wp-cli/handbook/master/bin/commands-manifest.json';
+	private static $commands_manifest = 'https://raw.githubusercontent.com/wp-cli/handbook/main/bin/commands-manifest.json';
 	private static $meta_key = 'wporg_cli_markdown_source';
 	private static $supported_post_types = array( 'command' );
 	private static $posts_per_page = -1;
