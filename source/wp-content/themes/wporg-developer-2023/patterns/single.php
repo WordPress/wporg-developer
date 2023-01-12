9a6ea2119ade5c6b72d4f09c1f5d8c0cd07ddfbe
dufresnesteven <steve.dufresne@automattic.com>
2023-01-12 12:00:19 +0900

    Add explanation and titles to single.

--- source/wp-content/themes/wporg-developer-2023/patterns/single.php
+++ source/wp-content/themes/wporg-developer-2023/patterns/single.php
@@ -9,15 +9,43 @@
 
 <!-- wp:group {"tagName":"main","style":{"spacing":{"blockGap":"0px","padding":{"top":"var:preset|spacing|50"}}},"className":"entry-content","layout":{"type":"constrained"}} -->
 <main class="wp-block-group entry-content" style="padding-top:var(--wp--preset--spacing--50);">
-<!-- wp:post-content /-->
 <!-- wp:wporg/code-reference-title /-->
 <!-- wp:wporg/code-reference-summary /-->
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Description</h2>
+<!-- /wp:heading -->
 <!-- wp:wporg/code-reference-description /-->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Parameters</h2>
+<!-- /wp:heading -->
 <!-- wp:wporg/code-reference-parameters /-->
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Return</h2>
+<!-- /wp:heading -->
 <!-- wp:wporg/code-reference-return-value /-->
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Explanation</h2>
+<!-- /wp:heading -->
+<!-- wp:wporg/code-reference-explanation /-->
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Source</h2>
+<!-- /wp:heading -->
 <!-- wp:wporg/code-reference-source /-->
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Hooks</h2>
+<!-- /wp:heading -->
 <!-- wp:wporg/code-reference-hooks /-->
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Related</h2>
+<!-- /wp:heading -->
 <!-- wp:wporg/code-reference-related /-->
+<!-- wp:heading -->
+<h2 class="wp-block-heading">Changelog</h2>
+<!-- /wp:heading -->
 <!-- wp:wporg/code-reference-changelog /-->
+<!-- wp:heading -->
+<h2 class="wp-block-heading">User Contributed Notes</h2>
+<!-- /wp:heading -->
 <!-- wp:wporg/code-reference-user-notes /--></main>
 <!-- /wp:group -->
