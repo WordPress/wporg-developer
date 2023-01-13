03f8bae4cfc9d63d4bd75737cd6caca27d5c7ab9
dufresnesteven <steve.dufresne@automattic.com>
2023-01-13 15:09:24 +0900

    Add in the version line item.

--- source/wp-content/themes/wporg-developer-2023/patterns/front-page-content.php
+++ source/wp-content/themes/wporg-developer-2023/patterns/front-page-content.php
@@ -1,33 +1,38 @@
 <?php
 /**
- * Title: Front Page Contene
+ * Title: Front Page Content
  * Slug: wporg-developer-2023/front-page-content
  * Inserter: no
  */
 
 ?>
 
-<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"backgroundColor":"charcoal-2","textColor":"white","layout":{"type":"constrained","wideSize":"1320px"}} -->
-<div class="wp-block-group alignfull has-white-color has-charcoal-2-background-color has-text-color has-background has-link-color"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"default"}} -->
-<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:image {"align":"full","id":7564,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image alignfull size-full"><img src="http://localhost:8888/wp-content/uploads/2023/01/developers.svg" alt="<?php esc_attr_e( 'WordPress showcase', 'wporg' ); ?>"/></figure>
+<!-- wp:group {"className":"alignfull has-white-color has-charcoal-2-background-color has-text-color has-background has-link-color","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-white-color has-charcoal-2-background-color has-text-color has-background has-link-color"><!-- wp:image {"align":"full","id":7564,"sizeSlug":"full","linkDestination":"none","className":"wporg-hero-graphic"} -->
+<figure class="wp-block-image alignfull size-full wporg-hero-graphic"><img src="http://localhost:8888/wp-content/uploads/2023/01/developers.svg" alt="WordPress showcase" class="wp-image-7564"/></figure>
 <!-- /wp:image -->
-<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|edge-space"}}},"layout":{"type":"default"}} -->
-<div class="wp-block-group alignwide" style="padding-left:var(--wp--preset--spacing--edge-space);padding-right:var(--wp--preset--spacing--edge-space)">
-<!-- wp:search {"label":"Search","showLabel":false,"width":432,"widthUnit":"px","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"placeholder":"Search in the code reference...","className":"is-style-secondary-search-control"} /--></div>
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|edge-space"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--edge-space)"><!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search in the code reference...","width":432,"widthUnit":"px","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
+<!-- wp:group {"backgroundColor":"blueberry-4","style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-blueberry-4-background-color has-background" style="padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10)"><!-- wp:paragraph {"align":"center","fontSize":"extra-small"}  -->
+<p class="has-text-align-center has-extra-small-font-size">See <a href="[wordpress_version_link]">what has changed</a>	in the WordPress [wordpress_version] api.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
 
-<!-- wp:post-content {"layout":{"inherit":true},"style":{"spacing":{"blockGap":"0px"}}} /-->
+<!-- wp:post-content {"style":{"spacing":{"blockGap":"0px"}},"layout":{"inherit":true}} /-->
 
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space"}}},"backgroundColor":"blueberry-4","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-blueberry-4-background-color" id="news" style="padding-right:var(--wp--preset--spacing--edge-space);padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"top":"5rem","bottom":"5rem"},"blockGap":"0px"}},"className":"is-style-default"} -->
+<div class="wp-block-group alignfull has-blueberry-4-background-color has-background" id="news" style="padding-right:var(--wp--preset--spacing--edge-space);padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"top":"5rem","bottom":"5rem"},"blockGap":"0px"}},"className":"is-style-default"} -->
 <div class="wp-block-columns alignwide is-style-default" style="padding-top:5rem;padding-bottom:5rem"><!-- wp:column {"verticalAlignment":"top","width":"33%","className":"is-left-column","layout":{"inherit":false}} -->
 <div class="wp-block-column is-vertically-aligned-top is-left-column" style="flex-basis:33%"><!-- wp:group {"style":{"spacing":{"padding":{"right":"80px"}}},"layout":{"type":"default"}} -->
 <div class="wp-block-group" style="padding-right:80px"><!-- wp:heading {"fontSize":"heading-2"} -->
-<h2 class="wp-block-heading has-heading-2-font-size"><?php esc_attr_e( 'Latest Posts', 'wporg' ); ?></h2>
+<h2 class="wp-block-heading has-heading-2-font-size">Latest Posts</h2>
 <!-- /wp:heading -->
 
 <!-- wp:spacer {"height":"20px"} -->
@@ -38,17 +43,16 @@
 
 <!-- wp:column {"width":"66%","layout":{"inherit":false}} -->
 <div class="wp-block-column" style="flex-basis:66%"><!-- wp:group {"style":{"spacing":{"margin":{"top":"15px"}}}} -->
-<div class="wp-block-group" style="margin-top:15px"><!-- wp:wporg/latest-news {"blogId":719} /-->
+<div class="wp-block-group" style="margin-top:15px"><!-- wp:wporg/latest-news /-->
 
 <!-- wp:spacer {"height":"20px"} -->
 <div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
 <!-- /wp:spacer -->
 
 <!-- wp:paragraph -->
-<p><a href="https://developer.wordpress.org/news/"><?php esc_attr_e( 'View all', 'wporg' ); ?></a></p>
+<p><a href="https://developer.wordpress.org/news/">View all</a></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
 <!-- /wp:group -->
-
