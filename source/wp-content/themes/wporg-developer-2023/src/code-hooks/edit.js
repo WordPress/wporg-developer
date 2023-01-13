4b97fac04f37d8b9d4019dd20212b2e4cc0dd64e
dufresnesteven <steve.dufresne@automattic.com>
2023-01-13 09:33:01 +0900

    Remove the block props code on the components.

--- source/wp-content/themes/wporg-developer-2023/src/code-hooks/edit.js
+++ source/wp-content/themes/wporg-developer-2023/src/code-hooks/edit.js
@@ -1,10 +1,5 @@
-import { useBlockProps } from '@wordpress/block-editor';
 import ServerSideRender from '@wordpress/server-side-render';
 
 export default function Edit( { name, attributes } ) {
-	return (
-		<p { ...useBlockProps() }>
-			<ServerSideRender block={ name } attributes={ attributes } />
-		</p>
-	);
+	return <ServerSideRender block={ name } attributes={ attributes } />;
 }
