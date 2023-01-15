ecd2e662f75fcf5281992cb476b5d0700bd0e7d4
dufresnesteven <steve.dufresne@automattic.com>
2023-01-15 11:50:49 +0900

    Update shortcodes to check for version.

--- source/wp-content/themes/wporg-developer-2023/inc/shortcodes.php
+++ source/wp-content/themes/wporg-developer-2023/inc/shortcodes.php
@@ -21,7 +21,7 @@ add_shortcode(
 	'wordpress_version_link',
 	function() {
 		$version = get_current_version_term();
-		return esc_attr( get_term_link( $version, 'wp-parser-since' ) );
+		return $version && ! is_wp_error( $version ) ? esc_attr( get_term_link( $version, 'wp-parser-since' ) ) : '';
 	}
 );
 
