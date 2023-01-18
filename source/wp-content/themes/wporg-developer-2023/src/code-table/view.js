1beccfec3d684f63717f34172094f7df75384697
dufresnesteven <steve.dufresne@automattic.com>
2023-01-18 15:29:07 +0900

    Check for links before binding.

--- source/wp-content/themes/wporg-developer-2023/src/code-table/view.js
+++ source/wp-content/themes/wporg-developer-2023/src/code-table/view.js
@@ -5,19 +5,23 @@
 const init = () => {
 	const tables = document.querySelectorAll( '.wp-block-wporg-code-table' );
 	if ( tables ) {
-		tables.forEach( ( container ) => {
-			const showMoreLink = container.querySelector( '.wp-block-wporg-code-table-show-more' );
-			const showLessLink = container.querySelector( '.wp-block-wporg-code-table-show-less' );
+		tables.forEach( ( table ) => {
+			const showMoreLink = table.querySelector( '.wp-block-wporg-code-table-show-more' );
+			const showLessLink = table.querySelector( '.wp-block-wporg-code-table-show-less' );
 
-			showMoreLink.addEventListener( 'click', ( event ) => {
-				event.preventDefault();
-				container.classList.add( 'expanded' );
-			} );
+			if ( showMoreLink ) {
+				showMoreLink.addEventListener( 'click', ( event ) => {
+					event.preventDefault();
+					table.classList.add( 'expanded' );
+				} );
+			}
 
-			showLessLink.addEventListener( 'click', ( event ) => {
-				event.preventDefault();
-				container.classList.remove( 'expanded' );
-			} );
+			if ( showLessLink ) {
+				showLessLink.addEventListener( 'click', ( event ) => {
+					event.preventDefault();
+					table.classList.remove( 'expanded' );
+				} );
+			}
 		} );
 	}
 };
