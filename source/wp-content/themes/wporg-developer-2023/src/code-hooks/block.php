544bd0d57f30e49dac37ab31c3b6c093e0672eb5
Steven Dufresne <steve.dufresne@automattic.com>
2023-01-27 09:00:40 +0900

    Block: Add code hooks. (#175)

--- source/wp-content/themes/wporg-developer-2023/src/code-hooks/block.php
+++ source/wp-content/themes/wporg-developer-2023/src/code-hooks/block.php
@@ -4,6 +4,7 @@ namespace WordPressdotorg\Theme\Developer_2023\Dynamic_Code_Hooks;
 use function DevHub\get_hooks;
 use function DevHub\post_type_has_hooks_info;
 use function DevHub\get_signature;
+use function DevHub\get_summary;
 
 add_action( 'init', __NAMESPACE__ . '\init' );
 
@@ -29,11 +30,24 @@ function init() {
  * @return string Returns the block markup.
  */
 function render() {
-	$has_hooks   = ( post_type_has_hooks_info() && ( $hooks   = get_hooks() ) && $hooks->have_posts() );
-
-	if ( ! $has_hooks ) {
+	if ( ! post_type_has_hooks_info() ) {
 		return '';
 	}
+
+	$hooks = get_hooks();
+
+	if ( ! $hooks->have_posts() ) {
+		return '';
+	}
+
+	$content = array();
+	while ( $hooks->have_posts() ) {
+		$hooks->the_post();
+
+		$content[] = do_blocks( '<!-- wp:wporg/code-reference-title {"isLink":true,"tagName":"dt","fontSize":"normal"} /-->' );
+		$content[] = '<dd>' . get_summary() . '</dd>';
+	}
+	wp_reset_postdata();
 
 	$title_block = sprintf(
 		'<h2 class="wp-block-heading">%s</h2>',
@@ -42,9 +56,9 @@ function render() {
 
 	$wrapper_attributes = get_block_wrapper_attributes();
 	return sprintf(
-		'<section %s>%s %s</section>',
+		'<section %s>%s <dl>%s</dl></section>',
 		$wrapper_attributes,
 		do_blocks( $title_block ),
-		'Not Implemented'
+		join( '', $content )
 	);
 }
