fff2816c64e4b429b59f379e36473f1d8d2ea38c
dufresnesteven <steve.dufresne@automattic.com>
2023-02-06 07:39:03 +0900

    Remove duplicate css.

--- source/wp-content/themes/wporg-developer-2023/src/search-results-context/index.js
+++ source/wp-content/themes/wporg-developer-2023/src/search-results-context/index.js
@@ -8,7 +8,6 @@ import { useBlockProps } from '@wordpress/block-editor';
  * Internal dependencies
  */
 import metadata from './block.json';
-import './style.scss';
 
 function Edit() {
 	return <div { ...useBlockProps() }>Search Results Context</div>;
