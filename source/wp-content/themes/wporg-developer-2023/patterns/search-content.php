fcedf19d8e7832e6fb199b3e127a345d4ca27750
dufresnesteven <steve.dufresne@automattic.com>
2023-02-07 10:03:27 +0900

    Add padding to search template.

--- source/wp-content/themes/wporg-developer-2023/patterns/search-content.php
+++ source/wp-content/themes/wporg-developer-2023/patterns/search-content.php
@@ -7,45 +7,41 @@
 
 ?>
 
-<!-- wp:group {"tagName":"main","style":{"spacing":{"blockGap":"0px","padding":{"top":"var:preset|spacing|50"}}},"className":"entry-content","layout":{"type":"constrained"}} -->
-<main class="wp-block-group entry-content" style="padding-top:var(--wp--preset--spacing--50);">
+<!-- wp:query {"queryId":0,"query":{"inherit":true,"perPage":10},"align":"wide"} -->
+<div class="wp-block-query alignwide">
 
-	<!-- wp:query {"queryId":0,"query":{"inherit":true,"perPage":10},"align":"wide"} -->
-	<div class="wp-block-query alignwide">
+<!-- wp:group {"className":"align-left","layout":{"type":"constrained","contentSize":"","justifyContent":"left"}} -->
+<div class="wp-block-group align-left">
 
-	<!-- wp:group {"className":"align-left","layout":{"type":"constrained","contentSize":"","justifyContent":"left"}} -->
-	<div class="wp-block-group align-left">
-
-	<!-- wp:group {"className":"awesomeplete-form-wrap search-wrap"} -->
-	<div class="wp-block-group awesomeplete-form-wrap search-wrap">
-	<!-- wp:search {"label":"Search","showLabel":false,"buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"className":"is-style-secondary-search-control wporg-filtered-search-form"} /--></div>
-	<!-- /wp:group -->
-
-	<!-- wp:wporg/search-results-context {"style":{"spacing":{"padding":{"top":"var:preset|spacing|20"}}}} /-->
-
-	<!-- wp:post-template -->
-	
-	<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}}}} -->
-	<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10)">	
-	<!-- wp:wporg/search-title /-->
-	<!-- wp:post-excerpt /-->
-	<!-- wp:wporg/search-usage-info {"style":{"elements":{"link":{"color":{"text":"var:preset|color|charcoal-4"}}}},"textColor":"charcoal-4","fontSize":"small"} /--></div>
-	<!-- /wp:group -->
-
-	<!-- /wp:post-template -->
-		<!-- wp:query-no-results -->
-	<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results.","style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}}}} -->
-	<p style="margin-top:var(--wp--preset--spacing--40)"><?php esc_attr_e( 'Sorry, but nothing matched your search terms.', 'wporg' ); ?></p>
-	<!-- /wp:paragraph -->
-	<!-- /wp:query-no-results --></div>
-	<!-- /wp:group -->
-
-	<!-- wp:query-pagination {"layout":{"type":"flex","justifyContent":"center"}} -->
-	<!-- wp:query-pagination-previous /-->
-
-	<!-- wp:query-pagination-numbers /-->
-
-	<!-- wp:query-pagination-next /-->
-	<!-- /wp:query-pagination --></div>
-	<!-- /wp:query --></main>
+<!-- wp:group {"className":"awesomeplete-form-wrap search-wrap"} -->
+<div class="wp-block-group awesomeplete-form-wrap search-wrap">
+<!-- wp:search {"label":"Search","showLabel":false,"buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"className":"is-style-secondary-search-control wporg-filtered-search-form"} /--></div>
 <!-- /wp:group -->
+
+<!-- wp:wporg/search-results-context {"style":{"spacing":{"padding":{"top":"var:preset|spacing|20"}}}} /-->
+
+<!-- wp:post-template -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}}}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10)">	
+<!-- wp:wporg/search-title /-->
+<!-- wp:post-excerpt /-->
+<!-- wp:wporg/search-usage-info {"style":{"elements":{"link":{"color":{"text":"var:preset|color|charcoal-4"}}}},"textColor":"charcoal-4","fontSize":"small"} /--></div>
+<!-- /wp:group -->
+
+<!-- /wp:post-template -->
+	<!-- wp:query-no-results -->
+<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results.","style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}}}} -->
+<p style="margin-top:var(--wp--preset--spacing--40)"><?php esc_attr_e( 'Sorry, but nothing matched your search terms.', 'wporg' ); ?></p>
+<!-- /wp:paragraph -->
+<!-- /wp:query-no-results --></div>
+<!-- /wp:group -->
+
+<!-- wp:query-pagination {"layout":{"type":"flex","justifyContent":"center"}} -->
+<!-- wp:query-pagination-previous /-->
+
+<!-- wp:query-pagination-numbers /-->
+
+<!-- wp:query-pagination-next /-->
+<!-- /wp:query-pagination --></div>
+<!-- /wp:query -->
