9fea310bff3feced6b60d3f1c6c585b3d1749ee6
dufresnesteven <steve.dufresne@automattic.com>
2023-02-08 11:04:21 +0900

    Add a php pattern and a comment to speak to the filtering.

---
+++ source/wp-content/themes/wporg-developer-2023/patterns/single.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Title: Single Content
+ * Slug: wporg-developer-2023/single-content
+ * Inserter: no
+ */
+
+ // This content in filtered by `filter_code_content` in functions.php to add the post content.
+ // See: https://github.com/WordPress/wporg-developer/pull/194
+?>
+
+<!-- wp:post-content {"layout":{"inherit":true}} /-->
