e374b8c4882b41bdd9a26f55afab3731057d609f
dufresnesteven <steve.dufresne@automattic.com>
2023-02-08 15:42:40 +0900

    Server side render the dropdown.

--- source/wp-content/themes/wporg-developer-2023/src/version-select/index.js
+++ source/wp-content/themes/wporg-developer-2023/src/version-select/index.js
@@ -2,17 +2,13 @@
  * WordPress dependencies
  */
 import { registerBlockType } from '@wordpress/blocks';
-import { useBlockProps } from '@wordpress/block-editor';
 
 /**
  * Internal dependencies
  */
+import Edit from '../shared/dynamic-edit';
 import metadata from './block.json';
 import './style.scss';
-
-function Edit() {
-	return <div { ...useBlockProps() }>Version Select</div>;
-}
 
 registerBlockType( metadata.name, {
 	edit: Edit,
