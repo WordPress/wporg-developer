33e970801f48bca0f03801c0d519f86d509550bb
ren <18050944+renintw@users.noreply.github.com>
2023-02-10 04:36:12 +0800

    Pattern: Command-Meta

---
+++ source/wp-content/themes/wporg-developer-2023/patterns/command-meta.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Command Meta
+ * Slug: wporg-developer-2023/command-meta
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.88","fontStyle":"normal","fontWeight":"400"}},"textColor":"charcoal-1","fontSize":"normal","fontFamily":"inter"} -->
+<p class="has-charcoal-1-color has-text-color has-inter-font-family has-normal-font-size" style="font-style:normal;font-weight:400;line-height:1.88">
+	<em><?php esc_html_e( 'Command documentation is regenerated at every release. To add or update an example, please submit a pull request against the corresponding part of the codebase.', 'wporg' ); ?></em>
+</p>
+<!-- /wp:paragraph -->
