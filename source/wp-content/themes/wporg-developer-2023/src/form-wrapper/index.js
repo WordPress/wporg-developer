3404fbdd002df271d047bc3c2e97aff1575aab3e
dufresnesteven <steve.dufresne@automattic.com>
2023-02-10 14:36:12 +0900

    Clean up the form-wrapper.

--- source/wp-content/themes/wporg-developer-2023/src/form-wrapper/index.js
+++ source/wp-content/themes/wporg-developer-2023/src/form-wrapper/index.js
@@ -2,29 +2,21 @@
  * WordPress dependencies
  */
 import { registerBlockType } from '@wordpress/blocks';
-import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 
 /**
  * Internal dependencies
  */
 import metadata from './block.json';
+import Edit from './edit';
+import Save from './save';
 
 registerBlockType( metadata.name, {
 	/**
 	 * @see ./edit.js
 	 */
-	edit: () => {
-		return (
-			<div { ...useBlockProps() }>
-				<InnerBlocks />
-			</div>
-		);
-	},
-	save: () => {
-		return (
-			<div { ...useBlockProps.save() }>
-				<InnerBlocks.Content />
-			</div>
-		);
-	},
+	edit: Edit,
+	/**
+	 * @see ./save.js
+	 */
+	save: Save,
 } );
