c42e53a061a93e575324645fd53c8491ee191214
dufresnesteven <steve.dufresne@automattic.com>
2023-02-13 11:26:01 +0900

    share tables.

--- source/wp-content/themes/wporg-developer-2023/src/command-subcommand/index.js
+++ source/wp-content/themes/wporg-developer-2023/src/command-subcommand/index.js
@@ -8,6 +8,7 @@ import { registerBlockType } from '@wordpress/blocks';
  */
 import Edit from '../shared/dynamic-edit';
 import metadata from './block.json';
+import './style.scss';
 
 registerBlockType( metadata.name, {
 	/**
