64769c245194b9e735afeea2d59b1d20aa95bdd9
Adam Wood <adam.wood@automattic.com>
2023-02-13 13:03:45 +1300

    Add WP-CLI landing page (#182)

---
+++ source/wp-content/themes/wporg-developer-2023/patterns/cli-commands.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Title: CLI Commands Page Content
+ * Slug: wporg-developer-2023/cli-commands
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"left":"var:preset|spacing|edge-space","right":"var:preset|spacing|edge-space","top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"33%"} -->
+<div class="wp-block-column" style="flex-basis:33%"><!-- wp:heading {"level":1,"fontSize":"heading-2"} -->
+<h1 class="wp-block-heading has-heading-2-font-size">WP-CLI Commands</h1>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66%"} -->
+<div class="wp-block-column" style="flex-basis:66%"><!-- wp:paragraph -->
+<p>Below is a listing of all currently available WP-CLI commands with links to documentation on usage and subcommands.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Looking to learn more about the internal API of WP-CLI or to contribute to its development? Check out the WP-CLI team’s <a href="https://make.wordpress.org/cli/handbook/">handbook</a> and the <a href="https://make.wordpress.org/cli/">WP-CLI Blog</a>.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:wporg/cli-command-table /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
