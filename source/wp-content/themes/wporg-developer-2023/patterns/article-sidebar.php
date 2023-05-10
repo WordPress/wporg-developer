ed282e9af3e2a19c7f17cf09d7704e82ee122794
Kelly Dwan <ryelle@users.noreply.github.com>
2023-05-10 16:04:31 -0400

    Templates: Use the new global Sidebar Container block (#246)
    * Sidebar: Use the sidebar container block
    
    This provides the basic 2-column layout for articles, so we can remove the duplicate code from this theme.
    
    * Sidebar: Set search box to 100% wide
    
    Fixes #242

--- source/wp-content/themes/wporg-developer-2023/patterns/article-sidebar.php
+++ source/wp-content/themes/wporg-developer-2023/patterns/article-sidebar.php
@@ -7,14 +7,8 @@
 
 ?>
 
-<!-- wp:group {"tagName":"aside","className":"sidebar-container"} -->
-<aside class="wp-block-group sidebar-container">
-	<!-- wp:search {"label":"<?php esc_attr_e( 'Search', 'wporg' ); ?>","showLabel":false,"widthUnit":"px","buttonText":"<?php esc_attr_e( 'Search', 'wporg' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true,"className":"is-style-secondary-search-control","borderColor":"light-grey-1","placeholder":"<?php esc_attr_e( 'Search in the documentation', 'wporg' ); ?>"} /-->
+<!-- wp:wporg/sidebar-container -->
+	<!-- wp:search {"label":"<?php esc_attr_e( 'Search', 'wporg' ); ?>","showLabel":false,"width":100,"widthUnit":"%","buttonText":"<?php esc_attr_e( 'Search', 'wporg' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true,"className":"is-style-secondary-search-control","borderColor":"light-grey-1","placeholder":"<?php esc_attr_e( 'Search in the documentation', 'wporg' ); ?>"} /-->
 
-	<!-- wp:wporg/table-of-contents {"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20"}}}} /-->
-
-	<!-- wp:paragraph {"fontSize":"small","className":"is-link-to-top"} -->
-	<p class="has-small-font-size is-link-to-top"><a href="#wp--skip-link--target"><?php esc_html_e( '↑ Back to top', 'wporg' ); ?></a></p>
-	<!-- /wp:paragraph -->
-</aside>
-<!-- /wp:group -->
+	<!-- wp:wporg/table-of-contents {"style":{"elements":{"link":{"color":{"text":"var:preset|color|blueberry-1"}}},"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20"}}},"backgroundColor":"blueberry-4","textColor":"blueberry-1"} /-->
+<!-- /wp:wporg/sidebar-container -->
