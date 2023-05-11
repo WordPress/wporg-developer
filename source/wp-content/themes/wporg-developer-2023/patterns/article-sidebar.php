57fa8cc148a1d20164ba2c03439e979cdaee1372
Kelly Dwan <ryelle@users.noreply.github.com>
2023-05-11 12:39:12 -0400

    Patterns: Remove custom styles from Table of Contents block

--- source/wp-content/themes/wporg-developer-2023/patterns/article-sidebar.php
+++ source/wp-content/themes/wporg-developer-2023/patterns/article-sidebar.php
@@ -10,5 +10,5 @@
 <!-- wp:wporg/sidebar-container -->
 	<!-- wp:search {"label":"<?php esc_attr_e( 'Search', 'wporg' ); ?>","showLabel":false,"width":100,"widthUnit":"%","buttonText":"<?php esc_attr_e( 'Search', 'wporg' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true,"className":"is-style-secondary-search-control","borderColor":"light-grey-1","placeholder":"<?php esc_attr_e( 'Search in the documentation', 'wporg' ); ?>"} /-->
 
-	<!-- wp:wporg/table-of-contents {"style":{"elements":{"link":{"color":{"text":"var:preset|color|blueberry-1"}}},"spacing":{"padding":{"top":"var:preset|spacing|20","right":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20"}}},"backgroundColor":"blueberry-4","textColor":"blueberry-1"} /-->
+	<!-- wp:wporg/table-of-contents /-->
 <!-- /wp:wporg/sidebar-container -->
