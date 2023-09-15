6b3238914a1f69e86bb3798ab53495230707cc5a
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2023-09-15 17:17:05 +1200

    Add reference content

---
+++ source/wp-content/themes/wporg-developer-2023/patterns/reference-content.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Title: Reference Content
+ * Slug: wporg-developer-2023/reference-content
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|edge-space","right":"var:preset|spacing|edge-space"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--edge-space)">
+
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|20"}}}} -->
+	<div class="wp-block-columns alignwide">
+		
+		<!-- wp:column {"style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column">
+
+			<!-- wp:wporg/reference-new-updated /-->
+
+		</div>
+		<!-- /wp:column -->
+	
+		<!-- wp:column {"style":{"spacing":{"blockGap":"0"}}} -->
+		<div class="wp-block-column">
+			
+			<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+			<div class="wp-block-group">
+				
+				<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"normal","fontFamily":"inter"} -->
+				<h2 class="wp-block-heading has-inter-font-family has-normal-font-size" style="font-style:normal;font-weight:700">API reference</h2>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph -->
+				<p><a href="https://developer.wordpress.org/apis/">View all</a></p>
+				<!-- /wp:paragraph -->
+			
+			</div>
+			<!-- /wp:group -->
+
+			<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10","left":"var:preset|spacing|20","right":"var:preset|spacing|20"},"margin":{"top":"var:preset|spacing|20"}},"border":{"width":"1px"}},"borderColor":"light-grey-1","layout":{"type":"constrained"}} -->
+			<div class="wp-block-group has-border-color has-light-grey-1-border-color" style="border-width:1px;margin-top:var(--wp--preset--spacing--20);padding-top:var(--wp--preset--spacing--10);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--10);padding-left:var(--wp--preset--spacing--20)">
+			
+				<!-- wp:list {"style":{"typography":{"lineHeight":"3"},"spacing":{"padding":{"left":"0"}}},"className":"wporg-reference-list","fontSize":"small"} -->
+				<ul class="wporg-reference-list has-small-font-size" style="padding-left:0;line-height:3">
+				
+				<!-- wp:list-item -->
+				<li><a href="https://developer.wordpress.org/apis/handbook/dashboard-widgets/">Dashboard widgets</a></li>
+				<!-- /wp:list-item -->
+
+				<!-- wp:list-item -->
+				<li><a href="https://developer.wordpress.org/apis/handbook/database/">Database</a></li>
+				<!-- /wp:list-item -->
+
+				<!-- wp:list-item -->
+				<li><a href="https://developer.wordpress.org/apis/handbook/making-http-requests/">HTTP API</a></li>
+				<!-- /wp:list-item -->
+
+				<!-- wp:list-item -->
+				<li><a href="https://developer.wordpress.org/apis/handbook/filesystem/">Filesystem</a></li>
+				<!-- /wp:list-item -->
+
+				<!-- wp:list-item -->
+				<li><a href="https://developer.wordpress.org/apis/handbook/global-variables/">Global Variables</a></li>
+				<!-- /wp:list-item -->
+
+				<!-- wp:list-item -->
+				<li><a href="https://developer.wordpress.org/apis/handbook/metadata/">Metadata</a></li>
+				<!-- /wp:list-item -->
+
+				<!-- wp:list-item -->
+				<li><a href="https://developer.wordpress.org/apis/handbook/options/">Options</a></li>
+				<!-- /wp:list-item -->
+
+				<!-- wp:list-item -->
+				<li><a href="https://developer.wordpress.org/plugins/">Plugins</a></li>
+				<!-- /wp:list-item -->
+
+				<!-- wp:list-item -->
+				<li><a href="https://developer.wordpress.org/apis/handbook/quicktags/">Quicktags</a></li>
+				<!-- /wp:list-item -->
+
+				<!-- wp:list-item -->
+				<li><a href="https://developer.wordpress.org/rest-api/">REST API</a></li>
+				<!-- /wp:list-item -->
+
+				<!-- wp:list-item -->
+				<li><a href="https://developer.wordpress.org/apis/handbook/rewrite/">Rewrite</a></li>
+				<!-- /wp:list-item -->
+
+				<!-- wp:list-item -->
+				<li><a href="https://developer.wordpress.org/apis/handbook/settings/">Settings</a></li>
+				<!-- /wp:list-item -->
+
+				<!-- wp:list-item -->
+				<li><a href="https://developer.wordpress.org/apis/handbook/shortcode/">Shortcode</a></li>
+				<!-- /wp:list-item -->
+
+				<!-- wp:list-item -->
+				<li><a href="https://developer.wordpress.org/themes/">Theme Modification</a></li>
+				<!-- /wp:list-item -->
+
+				<!-- wp:list-item -->
+				<li><a href="https://developer.wordpress.org/apis/handbook/transients/">Transients</a></li>
+				<!-- /wp:list-item -->
+
+				<!-- wp:list-item -->
+				<li><a href="https://developer.wordpress.org/apis/handbook/xml-rpc/">XML-RPC</a></li>
+				<!-- /wp:list-item -->
+			
+			</ul>
+			<!-- /wp:list -->
+		
+		</div>
+		<!-- /wp:group -->
+		
+		</div>
+		<!-- /wp:column -->
+	
+	</div>
+	<!-- /wp:columns -->
+
+</div>
+<!-- /wp:group -->
---
+++ source/wp-content/themes/wporg-developer-2023/src/reference-new-updated/block.php
@@ -0,0 +1,97 @@
+<?php
+namespace WordPressdotorg\Theme\Developer_2023\Reference_New_Updated;
+
+use function DevHub\get_current_version_term;
+use function DevHub\get_parsed_post_types;
+
+add_action( 'init', __NAMESPACE__ . '\init' );
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/reference/functions/register_block_type/
+ */
+function init() {
+	register_block_type(
+		dirname( dirname( __DIR__ ) ) . '/build/reference-new-updated',
+		array(
+			'render_callback' => __NAMESPACE__ . '\render',
+		)
+	);
+}
+
+/**
+ * Render the block content.
+ *
+ * @param array    $attributes Block attributes.
+ * @param string   $content    Block default content.
+ * @param WP_Block $block      Block instance.
+ *
+ * @return string Returns the block markup.
+ */
+function render( $attributes, $content, $block ) {
+	$version = get_current_version_term();
+
+	if ( ! ( $version && ! is_wp_error( $version ) ) ) {
+		return '';
+	}
+
+	$list = new \WP_Query( array(
+		'posts_per_page' => 15,
+		'post_type'      => get_parsed_post_types(),
+		'orderby'        => 'title',
+		'order'          => 'ASC',
+		'tax_query'      => array( array(
+			'taxonomy' => 'wp-parser-since',
+			'field'    => 'ids',
+			'terms'    => $version->term_id,
+		) ),
+	) );
+
+	$content = '<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10","left":"var:preset|spacing|20","right":"var:preset|spacing|20"},"margin":{"top":"var:preset|spacing|20"}},"border":{"width":"1px"}},"borderColor":"light-grey-1","layout":{"type":"constrained"}} -->
+	<div class="wp-block-group has-border-color has-light-grey-1-border-color" style="border-width:1px;margin-top:var(--wp--preset--spacing--20);padding-top:var(--wp--preset--spacing--10);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--10);padding-left:var(--wp--preset--spacing--20)">
+
+		<!-- wp:list {"style":{"typography":{"lineHeight":"3"},"spacing":{"padding":{"left":"0"}}},"className":"wporg-reference-list","fontSize":"small"} -->
+		<ul class="wporg-reference-list has-small-font-size" style="padding-left:0;line-height:3">';
+	
+	while ( $list->have_posts() ) : $list->the_post();
+
+		$content .= sprintf(
+			'<!-- wp:list-item --><li><a href="%s">%s</a></li><!-- /wp:list-item -->',
+			esc_url( get_permalink() ),
+			esc_html( get_the_title() )
+		);
+
+	endwhile;
+
+	$content .= '</ul><!-- /wp:list --></div><!-- /wp:group -->';
+
+	$title_block = sprintf(
+		'<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+		<div class="wp-block-group">
+
+			<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"normal","fontFamily":"inter"} -->
+			<h2 class="wp-block-heading has-inter-font-family has-normal-font-size" style="font-style:normal;font-weight:700">%s</h2>
+			<!-- /wp:heading -->
+
+			<!-- wp:paragraph -->
+			<p><a href="%s">%s</a></p>
+			<!-- /wp:paragraph -->
+
+		</div>
+		<!-- /wp:group -->',
+		sprintf( __( 'New and updated in %s', 'wporg' ), substr( $version->name, 0, -2 ) ),
+		esc_attr( get_term_link( $version, 'wp-parser-since' ) ),
+		__( 'View all', 'wporg' )
+	);
+
+	$wrapper_attributes = get_block_wrapper_attributes();
+	return sprintf(
+		'<div %s>%s %s</div>',
+		$wrapper_attributes,
+		do_blocks( $title_block ),
+		do_blocks( $content )
+	);
+}
