2760ef9d7bfbd74630f25dc29072c300237548da
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2023-09-21 17:17:30 +1200

    Update single search placeholder

--- source/wp-content/themes/wporg-developer-2023/patterns/single-search.php
+++ source/wp-content/themes/wporg-developer-2023/patterns/single-search.php
@@ -7,4 +7,4 @@
 
 ?>
 
-<!-- wp:search {"label":"<?php esc_attr_e( 'Search', 'wporg' ); ?>","showLabel":false,"placeholder":"<?php esc_attr_e( 'Search in the documentation', 'wporg' ); ?>","width":260,"widthUnit":"px","buttonText":"<?php esc_attr_e( 'Search', 'wporg' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|charcoal-1"}}}},"backgroundColor":"light-grey-2","textColor":"charcoal-1"} /-->
+<!-- wp:search {"label":"<?php esc_attr_e( 'Search', 'wporg' ); ?>","showLabel":false,"placeholder":"<?php esc_attr_e( 'Search resources', 'wporg' ); ?>","width":260,"widthUnit":"px","buttonText":"<?php esc_attr_e( 'Search', 'wporg' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|charcoal-1"}}}},"backgroundColor":"light-grey-2","textColor":"charcoal-1"} /-->
