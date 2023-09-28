45be3519a9be43ea56e0d2cf287fc8dbd3ed9de1
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2023-09-28 15:11:58 +1300

    Make single search pattern more reusable
    Update name and add repeated group block wrapper

--- source/wp-content/themes/wporg-developer-2023/patterns/single-search.php
+++ source/wp-content/themes/wporg-developer-2023/patterns/single-search.php
@@ -1,10 +1,16 @@
 <?php
 /**
- * Title: Single Search
- * Slug: wporg-developer-2023/single-search
+ * Title: Search Field
+ * Slug: wporg-developer-2023/search-field
  * Inserter: no
  */
 
 ?>
 
-<!-- wp:search {"label":"<?php esc_attr_e( 'Search', 'wporg' ); ?>","showLabel":false,"placeholder":"<?php esc_attr_e( 'Search resources', 'wporg' ); ?>","width":260,"widthUnit":"px","buttonText":"<?php esc_attr_e( 'Search', 'wporg' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|charcoal-1"}}}},"backgroundColor":"light-grey-2","textColor":"charcoal-1"} /-->
+<!-- wp:group {"layout":{"type":"constrained","justifyContent":"left"},"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}}} -->
+<div class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20)">
+
+	<!-- wp:search {"label":"<?php esc_attr_e( 'Search', 'wporg' ); ?>","showLabel":false,"placeholder":"<?php esc_attr_e( 'Search resources', 'wporg' ); ?>","width":260,"widthUnit":"px","buttonText":"<?php esc_attr_e( 'Search', 'wporg' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|charcoal-1"}}}},"backgroundColor":"light-grey-2","textColor":"charcoal-1"} /-->
+
+</div>
+<!-- /wp:group -->
