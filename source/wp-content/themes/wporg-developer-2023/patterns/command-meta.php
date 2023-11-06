ce0a5e8b4a073102aa12eb8cfd3d140e19c7f68d
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2023-11-07 10:31:42 +1300

    Increase space around command meta
    Fixes https://github.com/WordPress/wporg-developer/issues/320

--- source/wp-content/themes/wporg-developer-2023/patterns/command-meta.php
+++ source/wp-content/themes/wporg-developer-2023/patterns/command-meta.php
@@ -7,8 +7,8 @@
 
 ?>
 
-<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.88","fontStyle":"normal","fontWeight":"400"}},"textColor":"charcoal-1","fontSize":"normal","fontFamily":"inter"} -->
-<p class="has-charcoal-1-color has-text-color has-inter-font-family has-normal-font-size" style="font-style:normal;font-weight:400;line-height:1.88">
+<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.88","fontStyle":"normal","fontWeight":"400"},"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}},"textColor":"charcoal-1","fontSize":"normal","fontFamily":"inter"} -->
+<p class="has-charcoal-1-color has-text-color has-inter-font-family has-normal-font-size" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20);font-style:normal;font-weight:400;line-height:1.88">
 	<em><?php esc_html_e( 'Command documentation is regenerated at every release. To add or update an example, please submit a pull request against the corresponding part of the codebase.', 'wporg' ); ?></em>
 </p>
 <!-- /wp:paragraph -->
