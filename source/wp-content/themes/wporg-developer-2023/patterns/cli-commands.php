8e226a38decdde2a097d2113ae009f42ee16c6bc
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2023-11-15 16:33:20 +1300

    Move search out of commands main landmark

--- source/wp-content/themes/wporg-developer-2023/patterns/cli-commands.php
+++ source/wp-content/themes/wporg-developer-2023/patterns/cli-commands.php
@@ -10,57 +10,55 @@
 <!-- wp:group {"tagName":"main","style":{"spacing":{"padding":{"left":"var:preset|spacing|edge-space","right":"var:preset|spacing|edge-space","bottom":"var:preset|spacing|60"}}},"className":"alignfull","layout":{"type":"constrained","wideSize":"1280px","contentSize":"680px"}} -->
 <main class="wp-block-group alignfull" style="padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--edge-space)">
 
-	<!-- wp:pattern {"slug":"wporg-developer-2023/search-field"} /-->
-
 	<!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|20"}}},"layout":{"type":"constrained","justifyContent":"left"}} -->
 	<div class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--20)">
 	
-		<!-- wp:group {"tagName":"article","style":{"spacing":{"blockGap":"var:preset|spacing|40","margin":{"top":"0px"}}}} -->
-		<article class="wp-block-group" style="margin-top:0px">
+		<!-- wp:group {"tagName":"article"} -->
+		<article class="wp-block-group">
 		
-		<!-- wp:pattern {"slug":"wporg-developer-2023/article-sidebar"} /-->
+			<!-- wp:pattern {"slug":"wporg-developer-2023/article-sidebar"} /-->
 
-		<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
-		<div class="wp-block-group alignwide">
+			<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
+			<div class="wp-block-group alignwide">
 
-			<!-- wp:pattern {"slug":"wporg-developer-2023/cli-commands-content"} /-->
+				<!-- wp:pattern {"slug":"wporg-developer-2023/cli-commands-content"} /-->
 
-			<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"className":"is-style-cards-grid","layout":{"type":"grid","minimumColumnWidth":"49%"}} -->
-			<div class="wp-block-group is-style-cards-grid">
+				<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"className":"is-style-cards-grid","layout":{"type":"grid","minimumColumnWidth":"49%"}} -->
+				<div class="wp-block-group is-style-cards-grid">
+					
+					<!-- wp:wporg/link-wrapper -->
+					<a class="wp-block-wporg-link-wrapper" href="https://make.wordpress.org/cli/">
+						
+						<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small","fontFamily":"inter"} -->
+						<h3 class="wp-block-heading has-inter-font-family has-small-font-size" style="font-style:normal;font-weight:700"><?php esc_html_e( 'CLI Blog', 'wporg' ); ?></h3>
+						<!-- /wp:heading -->
+
+						<!-- wp:paragraph {"fontSize":"small"} -->
+						<p class="has-small-font-size"><?php esc_html_e( 'Catch up on the latest on WP-CLI in the main updates blog.', 'wporg' ); ?></p>
+						<!-- /wp:paragraph -->
+
+					</a>
+					<!-- /wp:wporg/link-wrapper -->
+
+					<!-- wp:wporg/link-wrapper -->
+					<a class="wp-block-wporg-link-wrapper" href="https://make.wordpress.org/cli/handbook/">
+						
+						<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small","fontFamily":"inter"} -->
+						<h3 class="wp-block-heading has-inter-font-family has-small-font-size" style="font-style:normal;font-weight:700"><?php esc_html_e( 'CLI Handbook', 'wporg' ); ?></h3>
+						<!-- /wp:heading -->
+
+						<!-- wp:paragraph {"fontSize":"small"} -->
+						<p class="has-small-font-size"><?php esc_html_e( 'A collection of helpful guides and resources for using WP-CLI.', 'wporg' ); ?></p>
+						<!-- /wp:paragraph -->
+						
+					</a>
+					<!-- /wp:wporg/link-wrapper -->
 				
-				<!-- wp:wporg/link-wrapper -->
-				<a class="wp-block-wporg-link-wrapper" href="https://make.wordpress.org/cli/">
-					
-					<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small","fontFamily":"inter"} -->
-					<h3 class="wp-block-heading has-inter-font-family has-small-font-size" style="font-style:normal;font-weight:700"><?php esc_html_e( 'CLI Blog', 'wporg' ); ?></h3>
-					<!-- /wp:heading -->
+				</div>
+				<!-- /wp:group -->
 
-					<!-- wp:paragraph {"fontSize":"small"} -->
-					<p class="has-small-font-size"><?php esc_html_e( 'Catch up on the latest on WP-CLI in the main updates blog.', 'wporg' ); ?></p>
-					<!-- /wp:paragraph -->
-
-				</a>
-				<!-- /wp:wporg/link-wrapper -->
-
-				<!-- wp:wporg/link-wrapper -->
-				<a class="wp-block-wporg-link-wrapper" href="https://make.wordpress.org/cli/handbook/">
-					
-					<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small","fontFamily":"inter"} -->
-					<h3 class="wp-block-heading has-inter-font-family has-small-font-size" style="font-style:normal;font-weight:700"><?php esc_html_e( 'CLI Handbook', 'wporg' ); ?></h3>
-					<!-- /wp:heading -->
-
-					<!-- wp:paragraph {"fontSize":"small"} -->
-					<p class="has-small-font-size"><?php esc_html_e( 'A collection of helpful guides and resources for using WP-CLI.', 'wporg' ); ?></p>
-					<!-- /wp:paragraph -->
-					
-				</a>
-				<!-- /wp:wporg/link-wrapper -->
-			
 			</div>
 			<!-- /wp:group -->
-
-		</div>
-		<!-- /wp:group -->
 
 		</article>
 		<!-- /wp:group -->
