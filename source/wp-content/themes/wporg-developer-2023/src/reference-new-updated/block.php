172f166484fd8466767995a02e86aa8f0655b9d5
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2023-11-20 11:05:40 +1300

    Use IBM Plex Sans for headings (#377)
    * Replace usage of eb-garamond with ibm-plex-sans
    
    * Set default heading font to IBM Plex Sans
    
    * Update large h2 headings to use Plex Sans

--- source/wp-content/themes/wporg-developer-2023/src/reference-new-updated/block.php
+++ source/wp-content/themes/wporg-developer-2023/src/reference-new-updated/block.php
@@ -72,8 +72,8 @@ function render( $attributes, $content, $block ) {
 		'<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 		<div class="wp-block-group">
 
-			<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"large","fontFamily":"inter"} -->
-			<h2 class="wp-block-heading has-inter-font-family has-large-font-size" style="font-style:normal;font-weight:700">%s</h2>
+			<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontSize":"large","fontFamily":"ibm-plex-sans"} -->
+			<h2 class="wp-block-heading has-ibm-plex-sans-font-family has-large-font-size" style="font-style:normal;font-weight:600">%s</h2>
 			<!-- /wp:heading -->
 
 			<!-- wp:paragraph -->
