51e9d1183b52b816ee532d23a1af328a59dbd502
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2023-11-20 21:33:29 +1300

    Add keyboard event handlers

--- source/wp-content/themes/wporg-developer-2023/js/code-tabs.js
+++ source/wp-content/themes/wporg-developer-2023/js/code-tabs.js
@@ -9,6 +9,14 @@ const init = () => {
 		const buttons = tabList.querySelectorAll( '.code-tab' );
 		const panels = tabList.querySelectorAll( '.code-tab-block' );
 
+		if ( ! buttons.length || ! panels.length || buttons.length !== panels.length ) {
+			return;
+		}
+
+		tabList.setAttribute( 'role', 'tablist' );
+		// Block button styles require a parent with `class*="wp-block"`.
+		tabList.classList.add( 'wp-block', 'is-small', 'is-style-toggle' );
+
 		buttons.forEach( ( button ) => {
 			const isActive = button.classList.contains( 'is-active' );
 			const buttonId = window.crypto?.randomUUID?.();
@@ -33,36 +41,61 @@ const init = () => {
 			panel.setAttribute( 'role', 'tabpanel' );
 		} );
 
-		tabList.setAttribute( 'role', 'tablist' );
-		// Block button styles require a parent with `class*="wp-block"`.
-		tabList.classList.add( 'wp-block', 'is-small', 'is-style-toggle' );
+		const maybeActivateTab = ( currentButton ) => {
+			if ( currentButton.classList.contains( 'is-active' ) ) {
+				return;
+			}
+
+			const panelId = currentButton.getAttribute( 'aria-controls' );
+
+			// Update button states.
+			buttons.forEach( ( button ) => {
+				button.classList.remove( 'is-active' );
+				button.setAttribute( 'aria-selected', 'false' );
+				button.setAttribute( 'tabindex', '-1' );
+			} );
+			currentButton.classList.add( 'is-active' );
+			currentButton.setAttribute( 'aria-selected', 'true' );
+			currentButton.setAttribute( 'tabindex', '0' );
+
+			// Update panel states.
+			panels.forEach( ( panel ) => {
+				panel.classList.remove( 'is-active' );
+			} );
+			document.getElementById( panelId )?.classList.add( 'is-active' );
+		};
 
 		tabList.addEventListener( 'click', ( event ) => {
+			if ( ! Array.from( buttons ).includes( event.target ) ) {
+				return;
+			}
+
 			event.preventDefault();
+			maybeActivateTab( event.target );
+		} );
 
-			if ( Array.from( buttons ).includes( event.target ) ) {
-				const targetButton = event.target;
-				const id = targetButton.getAttribute( 'aria-controls' );
+		tabList.addEventListener( 'keydown', ( event ) => {
+			if ( ! Array.from( buttons ).includes( event.target ) ) {
+				return;
+			}
 
-				if ( targetButton.classList.contains( 'is-active' ) ) {
-					return;
+			if ( event.code === 'Enter' || event.code === 'Space' ) {
+				event.preventDefault();
+				maybeActivateTab( event.target );
+			} else if ( event.code === 'ArrowLeft' || event.code === 'ArrowRight' ) {
+				event.preventDefault();
+
+				// Cycle focus within the tab list.
+				const currentIndex = Array.from( buttons ).indexOf( event.target );
+				const isMovingRight = event.code === 'ArrowRight';
+				const nextIndex = isMovingRight ? currentIndex + 1 : currentIndex - 1;
+				const nextButton = buttons[ nextIndex ];
+
+				if ( nextButton ) {
+					nextButton.focus();
+				} else {
+					buttons[ isMovingRight ? 0 : buttons.length - 1 ].focus();
 				}
-
-				// Update button states.
-				buttons.forEach( ( button ) => {
-					button.classList.remove( 'is-active' );
-					button.setAttribute( 'aria-selected', 'false' );
-					button.setAttribute( 'tabindex', '-1' );
-				} );
-				targetButton.classList.add( 'is-active' );
-				targetButton.setAttribute( 'aria-selected', 'true' );
-				targetButton.setAttribute( 'tabindex', '0' );
-
-				// Update tab states.
-				panels.forEach( ( panel ) => {
-					panel.classList.remove( 'is-active' );
-				} );
-				document.getElementById( id )?.classList.add( 'is-active' );
 			}
 		} );
 	} );
