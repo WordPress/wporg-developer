3acf021216d8ba431236bbd95c2ebb840619ee15
ren <18050944+renintw@users.noreply.github.com>
2023-11-27 05:24:11 +0900

    Fix broken TOC when showing more Used by

--- source/wp-content/themes/wporg-developer-2023/src/code-table/view.js
+++ source/wp-content/themes/wporg-developer-2023/src/code-table/view.js
@@ -4,6 +4,7 @@ const init = () => {
 		tables.forEach( ( table ) => {
 			const showMoreLink = table.querySelector( '.wp-block-wporg-code-table-show-more' );
 			const showLessLink = table.querySelector( '.wp-block-wporg-code-table-show-less' );
+			const sidebarContainer = document.querySelector( '.wp-block-wporg-sidebar-container' );
 
 			if ( showMoreLink ) {
 				showMoreLink.addEventListener( 'click', ( event ) => {
@@ -16,6 +17,13 @@ const init = () => {
 				showLessLink.addEventListener( 'click', ( event ) => {
 					event.preventDefault();
 					table.classList.remove( 'expanded' );
+					if ( sidebarContainer && sidebarContainer.classList.contains( 'is-bottom-sidebar' ) ) {
+						// Ensures that the sidebar repositions correctly after collapsing.
+						// It addresses the issue where the sidebar stays at its expanded position
+						// due to retaining the 'is-bottom-sidebar' class and the 'top' property post-collapse.
+						sidebarContainer.classList.remove( 'is-bottom-sidebar' );
+						sidebarContainer.style.removeProperty( 'top' );
+					}
 				} );
 			}
 		} );
