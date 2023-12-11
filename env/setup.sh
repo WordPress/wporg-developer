665c66813a2e719dce243028e17c15bf0c18e131
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2023-12-11 13:02:40 +1300

    Fix page creation in setup scripts

--- env/setup.sh
+++ env/setup.sh
@@ -6,7 +6,8 @@ wp rewrite structure '/%year%/%monthnum%/%postname%/'
 wp rewrite flush
 
 # Create the pages required.
-wp post create --post_type=page --post_title=Dashicons --post_status=publish --post_name=dashicons --page_template=page-dashicons.html
+wp post create --post_type=page --post_title=Dashicons --post_status=publish --post_name=dashicons
+wp post create --post_type=page --post_title=Home --post_status=publish --post_name=home
 
 # Set the homepage to be the home page.
 wp option update page_on_front `wp post list --post_type=page --name=home --format=ids`
