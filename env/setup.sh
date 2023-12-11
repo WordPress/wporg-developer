00116dfb3d7bf71a8eef8126e96d64a6c1a176eb
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2023-12-11 13:30:56 +1300

    Add all the required pages to the setup script

--- env/setup.sh
+++ env/setup.sh
@@ -6,8 +6,10 @@ wp rewrite structure '/%year%/%monthnum%/%postname%/'
 wp rewrite flush
 
 # Create the pages required.
-wp post create --post_type=page --post_title=Dashicons --post_status=publish --post_name=dashicons
 wp post create --post_type=page --post_title=Home --post_status=publish --post_name=home
+wp post create --post_type=page --post_title=Reference --post_status=publish --post_name=reference
+wp post create --post_type=page --post_title=Resource --post_status=publish --post_name=resource
+wp post create --post_type=page --post_title=Dashicons --post_status=publish --post_name=dashicons --post_parent=`wp post list --post_type=page --name=resource --format=ids`
 
 # Set the homepage to be the home page.
 wp option update page_on_front `wp post list --post_type=page --name=home --format=ids`
