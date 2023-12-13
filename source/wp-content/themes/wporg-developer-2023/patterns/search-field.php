7ff393428d038afe44f5846c9925678ae59e7633
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2023-12-14 11:25:03 +1300

    Fix RTL layout (#427)
    * Fix code reference single RTL layout
    
    * Fix single command layout
    
    * Fix chapter list rtl layout
    
    * Replace all left and right properties with logical properties
    
    * Convert remaining rtl code to logical properties
    
    * Make left justified content rule apply to all nested blocks
    
    * Fix awesomplete dropdown alignment
    
    * Fix user notes comment author vcard layout
    
    * Fix source code alignment

--- source/wp-content/themes/wporg-developer-2023/patterns/search-field.php
+++ source/wp-content/themes/wporg-developer-2023/patterns/search-field.php
@@ -7,7 +7,7 @@
 
 ?>
 
-<!-- wp:group {"layout":{"type":"constrained","justifyContent":"left"},"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}}} -->
+<!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}}} -->
 <div id="wporg-search" class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20)">
 
 	<!-- wp:search {"label":"<?php esc_attr_e( 'Search', 'wporg' ); ?>","showLabel":false,"placeholder":"<?php esc_attr_e( 'Search resources', 'wporg' ); ?>","width":232,"widthUnit":"px","buttonText":"<?php esc_attr_e( 'Search', 'wporg' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true} /-->
