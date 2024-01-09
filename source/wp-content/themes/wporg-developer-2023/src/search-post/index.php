0262d457012eeb053c6e1e3a81db652a3e903128
Kelly Dwan <ryelle@users.noreply.github.com>
2024-01-09 15:43:12 -0500

    Search Results Block: Linter and readability improvements
    This updates the function names to use snake_case, adds basic function docs, and updates the whitespace around block HTML for better readability.

--- source/wp-content/themes/wporg-developer-2023/src/search-post/index.php
+++ source/wp-content/themes/wporg-developer-2023/src/search-post/index.php
@@ -28,36 +28,46 @@ function init() {
 	);
 }
 
-function renderPost() {
-	return do_blocks( '<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}},"border":{"width":"1px","color":"#d9d9d9","radius":"2px"}}} -->
-	<div class="wp-block-group has-border-color" style="border-color:#d9d9d9;border-width:1px;border-radius:2px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)">
-	
-		<!-- wp:wporg/code-short-title /-->
-
-		<!-- wp:post-excerpt /-->
-	
-	</div>
-	<!-- /wp:group -->' );
-}
-
-function renderCodeReferencePost() {
-	return do_blocks( '<!-- wp:group {"align":"wide","layout":{"type":"constrained","justifyContent":"left"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}}}} -->
-	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10)">
-	
-		<!-- wp:group {"layout":{"type":"constrained"}} -->
-		<div class="wp-block-group">
+/**
+ * Render the post to be shown in search results (default).
+ */
+function render_post() {
+	return do_blocks(
+		'<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}},"border":{"width":"1px","color":"#d9d9d9","radius":"2px"}}} -->
+		<div class="wp-block-group has-border-color" style="border-color:#d9d9d9;border-width:1px;border-radius:2px;padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)">
 
 			<!-- wp:wporg/code-short-title /-->
 
 			<!-- wp:post-excerpt /-->
 
-			<!-- wp:wporg/code-type-usage-info {"style":{"elements":{"link":{"color":{"text":"var:preset|color|charcoal-4"}}}},"textColor":"charcoal-4","fontSize":"small"} /-->
+		</div>
+		<!-- /wp:group -->'
+	);
+}
+
+/**
+ * Render the post to be shown in search results, when the post is a code reference item.
+ */
+function render_code_reference_post() {
+	return do_blocks(
+		'<!-- wp:group {"align":"wide","layout":{"type":"constrained","justifyContent":"left"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10"}}}} -->
+		<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--10);padding-bottom:var(--wp--preset--spacing--10)">
+
+			<!-- wp:group {"layout":{"type":"constrained"}} -->
+			<div class="wp-block-group">
+
+				<!-- wp:wporg/code-short-title /-->
+
+				<!-- wp:post-excerpt /-->
+
+				<!-- wp:wporg/code-type-usage-info {"style":{"elements":{"link":{"color":{"text":"var:preset|color|charcoal-4"}}}},"textColor":"charcoal-4","fontSize":"small"} /-->
+
+			</div>
+			<!-- /wp:group -->
 
 		</div>
-		<!-- /wp:group -->
-	
-	</div>
-	<!-- /wp:group --> ');
+		<!-- /wp:group -->'
+	);
 }
 
 /**
@@ -76,8 +86,8 @@ function render( $attributes, $content, $block ) {
 
 	$post_type = get_post_type( $block->context['postId'] );
 	$is_parsed_type = is_parsed_post_type( $post_type );
-	
-	$content_html = $is_parsed_type ? renderCodeReferencePost() : renderPost();
+
+	$content_html = $is_parsed_type ? render_code_reference_post() : render_post();
 
 	$wrapper_attributes = get_block_wrapper_attributes(
 		array(
