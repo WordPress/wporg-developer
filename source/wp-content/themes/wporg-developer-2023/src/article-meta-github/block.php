6729fc83aa3c4f28072e8d6528179ad10b607de5
ren <18050944+renintw@users.noreply.github.com>
2024-01-16 08:13:07 +0800

    revert "If not logged in, redirect to login page."
    This reverts commit bcf2a504758c5cb35b060cbff05d5f88ebada8f3.

--- source/wp-content/themes/wporg-developer-2023/src/article-meta-github/block.php
+++ source/wp-content/themes/wporg-developer-2023/src/article-meta-github/block.php
@@ -39,7 +39,7 @@ function render( $attributes, $content, $block ) {
 	}
 
 	$title = get_the_title( $post_id );
-	$link_url = is_user_logged_in() ? $attributes['linkURL'] : wp_login_url( get_permalink() );
+	$link_url = $attributes['linkURL'];
 
 	return sprintf(
 		do_blocks(
