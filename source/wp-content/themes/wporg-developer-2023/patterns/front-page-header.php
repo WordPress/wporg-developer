adc8f48f5e4adcebe9e12b5b515d47dc21a3bd72
Kelly Dwan <ryelle@users.noreply.github.com>
2024-02-06 14:18:16 -0500

    Update homepage header

--- source/wp-content/themes/wporg-developer-2023/patterns/front-page-header.php
+++ source/wp-content/themes/wporg-developer-2023/patterns/front-page-header.php
@@ -9,9 +9,7 @@
 
 <!-- wp:wporg/local-navigation-bar {"backgroundColor":"charcoal-2","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"},":hover":{"color":{"text":"var:preset|color|white"}}}}},"textColor":"white","fontSize":"small"} -->
 
-	<!-- wp:html -->
-	<div style="height:calc(var(--wp--custom--body--small--typography--line-height) * var(--wp--preset--font-size--small));" aria-hidden="true"></div>
-	<!-- /wp:html -->
+	<!-- wp:site-title {"level":0,"fontSize":"small","className":"wporg-local-navigation-bar__show-on-scroll"} /-->
 
 	<!-- wp:navigation {"icon":"menu","overlayBackgroundColor":"charcoal-2","overlayTextColor":"white","layout":{"type":"flex","orientation":"horizontal"},"fontSize":"small","menuSlug":"developer"} /-->
 
