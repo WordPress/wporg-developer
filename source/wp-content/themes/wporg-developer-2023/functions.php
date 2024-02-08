88b5d42761098676fd03d0a61f25f87cda6f214e
Kelly Dwan <ryelle@users.noreply.github.com>
2024-02-08 12:42:34 -0500

    Add EB Garamond to the preload font list

--- source/wp-content/themes/wporg-developer-2023/functions.php
+++ source/wp-content/themes/wporg-developer-2023/functions.php
@@ -489,7 +489,7 @@ function theme_scripts_styles() {
 		/* translators: Subsets can be any of cyrillic, cyrillic-ext, greek, greek-ext, vietnamese, latin, latin-ext. */
 		$subsets = _x( 'Latin', 'Heading font subsets, comma separated', 'wporg' );
 		// All headings.
-		global_fonts_preload( 'Inter', $subsets );
+		global_fonts_preload( 'EB Garamond, Inter', $subsets );
 	}
 
 	if ( function_exists( 'wporg_is_handbook' ) && wporg_is_handbook() ) {
