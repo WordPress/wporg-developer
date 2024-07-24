a59cbb9efcdf9f62818cb55c8fb8cb431d34a274
Jon Surrell <sirreal@users.noreply.github.com>
2024-07-24 15:08:41 +0200

    Add icon to url query param when selecting

--- source/wp-content/themes/wporg-developer-2023/src/dashicons-page/view.js
+++ source/wp-content/themes/wporg-developer-2023/src/dashicons-page/view.js
@@ -25,7 +25,11 @@ const { state } = IAPI.store( 'wporg/dashicons-page', {
 	},
 
 	handleIconClick: () => {
+		const icon = IAPI.getContext().icon;
 		state.selectedIcon = [ IAPI.getContext().icon ];
+		const url = new URL( document.location.href );
+		url.searchParams.set( 'icon', icon );
+		window.history.replaceState( undefined, undefined, url );
 	},
 
 	copyClickHandlers: {
