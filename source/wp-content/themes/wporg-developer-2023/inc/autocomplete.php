2de56492e14867f3b4f69357fe3873b46101d836
Jon Surrell <sirreal@users.noreply.github.com>
2024-07-29 11:08:44 +0200

    Autocomplete script motules and modernize
    - No jQuery
    - Use Script Modules
    - Use REST endpoint for request

--- source/wp-content/themes/wporg-developer-2023/inc/autocomplete.php
+++ source/wp-content/themes/wporg-developer-2023/inc/autocomplete.php
@@ -20,14 +20,37 @@ class DevHub_Search_Form_Autocomplete {
 	 * @access public
 	 */
 	public function init() {
-
-		add_action( 'wp_ajax_autocomplete', array( $this, 'autocomplete_data_update' ) );
-		add_action( 'wp_ajax_nopriv_autocomplete', array( $this, 'autocomplete_data_update' ) );
+		add_action(
+			'rest_api_init',
+			function () {
+				register_rest_route(
+					'wporg-developer/v1',
+					'/autocomplete',
+					array(
+						'methods'             => 'POST',
+						'callback'            => array( $this, 'autocomplete_rest_handler' ),
+						'permission_callback' => '__return_true',
+						'schema' => function () {
+							return array(
+								'html' => array(
+									'description' => 'The HTML to process.',
+									'type' => 'string',
+									'required' => true,
+								),
+								'quirksMode' => array(
+									'description' => 'Process the document in quirks mode.',
+									'type' => 'boolean',
+								),
+							);
+						},
+					)
+				);
+			}
+		);
 
 		// Enqueue scripts and styles.
 		add_action( 'wp_enqueue_scripts', array( $this, 'scripts_and_styles' ), 11 );
 	}
-
 
 	/**
 	 * Enqueues scripts and styles.
@@ -37,97 +60,117 @@ class DevHub_Search_Form_Autocomplete {
 	public function scripts_and_styles() {
 
 		// Handbook searches don't have autocomplete.
-		if ( function_exists( 'wporg_is_handbook' ) && wporg_is_handbook() ) {
-			return;
-		}
+		/*if ( function_exists( 'wporg_is_handbook' ) && wporg_is_handbook() ) {*/
+		/*	return;*/
+		/*}*/
+		/**/
+		/*wp_enqueue_style(*/
+		/*	'awesomplete-css',*/
+		/*	get_stylesheet_directory_uri() . '/stylesheets/awesomplete.css',*/
+		/*	array(),*/
+		/*	filemtime( dirname( __DIR__ ) . '/stylesheets/awesomplete.css' )*/
+		/*);*/
+		/*wp_enqueue_style(*/
+		/*	'autocomplete-css',*/
+		/*	get_stylesheet_directory_uri() . '/stylesheets/autocomplete.css',*/
+		/*	array(),*/
+		/*	filemtime( dirname( __DIR__ ) . '/stylesheets/autocomplete.css' )*/
+		/*);*/
+		/**/
 
-		wp_enqueue_style(
-			'awesomplete-css',
-			get_stylesheet_directory_uri() . '/stylesheets/awesomplete.css',
-			array(),
-			filemtime( dirname( __DIR__ ) . '/stylesheets/awesomplete.css' )
-		);
-		wp_enqueue_style(
-			'autocomplete-css',
-			get_stylesheet_directory_uri() . '/stylesheets/autocomplete.css',
-			array(),
-			filemtime( dirname( __DIR__ ) . '/stylesheets/autocomplete.css' )
-		);
-
-		wp_register_script(
+		wp_enqueue_script(
 			'awesomplete',
 			get_stylesheet_directory_uri() . '/js/awesomplete.min.js',
 			array(),
 			filemtime( dirname( __DIR__ ) . '/js/awesomplete.min.js' ),
 			true
 		);
-		wp_enqueue_script( 'awesomplete' );
-
-		wp_register_script( 'autocomplete', get_stylesheet_directory_uri() . '/js/autocomplete.js', array( 'awesomplete' ), filemtime( dirname( __DIR__ ) . '/js/autocomplete.js' ), true );
-		wp_localize_script(
-			'autocomplete',
-			'autocomplete',
-			array(
-				'ajaxurl'   => admin_url( 'admin-ajax.php' ),
-				'nonce'     => wp_create_nonce( 'autocomplete_nonce' ),
-				'post_type' => get_post_type(),
-			)
+		wp_enqueue_script_module(
+			'@wporg-developer/autocomplete',
+			get_stylesheet_directory_uri() . '/js/autocomplete.js',
+			array(),
+			filemtime( get_stylesheet_directory() . '/js/autocomplete.js' ),
 		);
 
-		wp_enqueue_script( 'autocomplete' );
+		/*wp_register_script(*/
+		/*	'autocomplete',*/
+		/*	get_stylesheet_directory_uri() . '/js/autocomplete.js',*/
+		/*	array( 'awesomplete' ),*/
+		/*	filemtime( dirname( __DIR__ ) . '/js/autocomplete.js' ),*/
+		/*	true*/
+		/*);*/
+		/*wp_localize_script(*/
+		/*	'autocomplete',*/
+		/*	'autocomplete',*/
+		/*	array(*/
+		/*		'ajaxurl'   => admin_url( 'admin-ajax.php' ),*/
+		/*		'nonce'     => wp_create_nonce( 'autocomplete_nonce' ),*/
+		/*		'post_type' => get_post_type(),*/
+		/*	)*/
+		/*);*/
+
+		/*wp_enqueue_script( 'autocomplete' );*/
+
+		add_filter(
+			"script_module_data_@wporg-developer/autocomplete",
+			function ( $data ) {
+				return array_merge( $data, array(
+					'rest_url' => rest_url('wporg-developer/v1/autocomplete'),
+					'nonce'       => wp_create_nonce( 'wporg/autocomplete' ),
+					'post_type'   => get_post_type(),
+				) );
+			}
+		);
 	}
 
+	public function autocomplete_rest_handler( WP_REST_Request $request ) {
+		$req_json = $request->get_json_params();
 
-	/**
-	 * Handles AJAX updates for the autocomplete list.
-	 *
-	 * @access public
-	 *
-	 * @return string JSON data
-	 */
-	public function autocomplete_data_update() {
-
-		check_ajax_referer( 'autocomplete_nonce', 'nonce' );
-
-		$parser_post_types = DevHub\get_parsed_post_types();
-		$defaults          = array(
-			's'         => '',
-			'posts'     => array(),
-		);
-
-		if ( ! ( isset( $_POST['data'] ) && $_POST['data'] ) ) {
-			wp_send_json_error( $defaults );
+		$nonce_ok = wp_verify_nonce( $req_json['nonce'], 'wporg/autocomplete' );
+		if ( ! $nonce_ok ) {
+			return new WP_REST_Response( array( 'error' => __( 'Invalid nonce' ) ), 403 );
 		}
-
-		// Parse the search form fields.
-		wp_parse_str( $_POST['data'], $form_data );
-		$form_data = array_merge( $defaults, $form_data );
 
 		// No search query.
-		if ( empty( $form_data['s'] ) ) {
-			wp_send_json_error( $defaults );
+		$req_search = $req_json['s'] ?? '';
+		if ( '' === $req_search ) {
+			return new WP_REST_Response(
+				array( 'error' => __( 'Missing or invalid "s" search.', 'wporg' ) ),
+				400
+			);
 		}
 
-		$post_types = isset( $_POST['post_type'] ) && 'command' === $_POST['post_type'] ?
-			array( 'command' ) :
-			$parser_post_types;
+		$parser_post_types = DevHub\get_parsed_post_types();
+
+		$req_post_types = $req_json['post_types'];
+		if ( ! is_array( $req_post_types ) ) {
+			$req_post_types = array();
+		} else {
+			$req_post_types = array_intersect( $parser_post_types, $req_post_types );
+		}
+
+		if ( array() === $req_post_types ) {
+			$req_post_types = $parser_post_types;
+		}
 
 		$args = array(
 			'posts_per_page'       => -1,
-			'post_type'            => $post_types,
-			's'                    => $form_data['s'],
+			'post_type'            => $req_post_types,
+			's'                    => $req_search,
 			'orderby'              => '',
 			'search_orderby_title' => 1,
 			'order'                => 'ASC',
 			'_autocomplete_search' => true,
 		);
 
-		$search = get_posts( $args );
+		$search_result = get_posts( $args );
 
-		if ( ! empty( $search ) ) {
+		if ( empty( $search_result ) ) {
+			return new WP_REST_Response( null, 200 );
+		} else {
 			$post_types_function_like = array( 'wp-parser-function', 'wp-parser-method' );
 
-			foreach ( $search as $post ) {
+			foreach ( $search_result as $post ) {
 				$permalink = get_permalink( $post->ID );
 				$title     = $post->post_title;
 
@@ -139,13 +182,12 @@ class DevHub_Search_Form_Autocomplete {
 					$title = 'class ' . $title . ' {}';
 				}
 
-				$form_data['posts'][ $title ] = $permalink;
+				$request_data['posts'][ $title ] = $permalink;
 			}
 		}
 
-		wp_send_json_success( $form_data );
+		return new WP_REST_Response( $request_data, 200 );
 	}
-
 }
 
 $autocomplete = new DevHub_Search_Form_Autocomplete();
--- source/wp-content/themes/wporg-developer-2023/js/autocomplete.js
+++ source/wp-content/themes/wporg-developer-2023/js/autocomplete.js
@@ -5,26 +5,43 @@
  * https://leaverou.github.io/awesomplete/
  */
 
-( function( $ ) {
+function init() {
+	/** @type {string} */
+	const stringifiedData = document.getElementById(
+		'wp-script-module-data-@wporg-developer/autocomplete'
+	)?.textContent;
 
-	if ( typeof autocomplete === 'undefined' ) {
+	let data;
+	try {
+		data = JSON.parse( stringifiedData );
+	} catch {
+		// No data from server, nothing to do.
 		return;
 	}
 
-	var form = $( '#wporg-search .wp-block-search' );
-	if ( ! form.length ) {
+	if ( typeof window.Awesomplete === 'undefined' ) {
 		return;
 	}
 
-	var	searchfield = $( 'input[type="search"]', form ),
-		processing = false,
+	const form = document.querySelector( '#wporg-search .wp-block-search' );
+	if ( ! form ) {
+		return;
+	}
+
+	/** @type {HTMLInputElement} */
+	const searchfield = form.querySelector( 'input[type="search"]' );
+	if ( ! searchfield ) {
+		return;
+	}
+
+	let processing = false,
 		search = '',
 		autocompleteResults = {};
 
-	var awesome = new Awesomplete( searchfield.get( 0 ), {
+	const awesome = new Awesomplete( searchfield, {
 		maxItems: 9999,
 		minChars: 3,
-		filter: function( text, input ) {
+		filter: function ( text, input ) {
 			// Filter autocomplete matches
 
 			// Full match
@@ -34,15 +51,25 @@
 			}
 
 			// Replace - _ and whitespace with a single space
-			var _text = Awesomplete.$.regExpEscape( text.trim().toLowerCase().replace( /[\_\-\s]+/g, ' ' ) );
-			var _input = Awesomplete.$.regExpEscape( input.trim().toLowerCase().replace( /[\_\-\s]+/g, ' ' ) );
+			var _text = Awesomplete.$.regExpEscape(
+				text
+					.trim()
+					.toLowerCase()
+					.replace( /[\_\-\s]+/g, ' ' )
+			);
+			var _input = Awesomplete.$.regExpEscape(
+				input
+					.trim()
+					.toLowerCase()
+					.replace( /[\_\-\s]+/g, ' ' )
+			);
 
 			// Matches with with single spaces between words
 			if ( Awesomplete.FILTER_CONTAINS( _text, _input ) ) {
 				return true;
 			}
 
-			_input = _input.split( " " );
+			_input = _input.split( ' ' );
 			var words = _input.length;
 
 			if ( 1 >= words ) {
@@ -63,68 +90,90 @@
 
 			return false;
 		},
-		replace: function( text ) {
+		replace: function ( text ) {
 			searchfield.val( text );
 
 			if ( text in autocompleteResults ) {
 				window.location = autocompleteResults[ text ];
 			}
-		}
+		},
 	} );
 
 	// On input event for the search field.
-	searchfield.on( 'input.autocomplete', function( e ) {
-
-		// Update the autocomlete list: 
+	searchfield.addEventListener( 'input', function ( e ) {
+		// Update the autocomplete list:
 		//     if there are more than 2 characters
 		//     and it's not already processing an Ajax request
-		if ( !processing && $( this ).val().trim().length > 2 ) {
-			search = $( this ).val();
+		if ( ! processing && this.value.trim().length > 2 ) {
+			search = this.value;
 			autocomplete_update();
 		}
 	} );
 
+	console.log( data, searchfield );
 
 	/**
 	 * Updates the autocomplete list
 	 */
-	function autocomplete_update() {
-
+	async function autocomplete_update() {
 		processing = true;
 
-		var data = {
-			action: "autocomplete",
-			data: form.serialize(),
-			nonce: autocomplete.nonce,
-			post_type: autocomplete.post_type
-		};
+		const url = new URL( data.rest_url, document.location.href );
 
-		$.post( autocomplete.ajaxurl, data )
-			.done( function( response ) {
+		const requestData = { s: '', post_types: [], nonce: data.nonce };
+		requestData.s = form.elements.namedItem( 's' )?.value ?? '';
+		for ( const postTypeEl of form.elements.namedItem( 'post_type[]' ) ) {
+			if ( postTypeEl.checked ) {
+				requestData.post_types.push( postTypeEl.value );
+			}
+		}
 
-				if ( typeof response.success === 'undefined' ) {
-					return false;
-				}
+		console.log( requestData );
 
-				if ( typeof response.data === 'undefined' ) {
-					return false;
-				}
-
-				if ( response.success === true && Object.values( response.data.posts ).length ) {
-					autocompleteResults = response.data.posts;
-
-					// Update the autocomplete list
-					awesome.list = Object.keys( response.data.posts );
-				}
-			} )
-			.always( function() {
-				processing = false;
-
-				// Check if the search was updated during processing
-				if ( search !== searchfield.val() ) {
-					searchfield.trigger( "input.autocomplete" );
-				}
+		/** @type {Response} */
+		let response;
+		try {
+			response = await fetch( url, {
+				method: 'POST',
+				body: JSON.stringify( requestData ),
+				headers: { 'Content-Type': 'application/json' },
 			} );
-	}
+			if ( ! response.ok ) {
+				throw new Error( 'Response not OK' );
+			}
+			response = await response.json();
+		} finally {
+			processing = false;
+		}
 
-} )( jQuery );
+		console.log( response );
+
+		//$.post( autocomplete.ajaxurl, data )
+		//	.done( function ( response ) {
+		//		if ( typeof response.success === 'undefined' ) {
+		//			return false;
+		//		}
+		//
+		//		if ( typeof response.data === 'undefined' ) {
+		//			return false;
+		//		}
+		//
+		//		if ( response.success === true && Object.values( response.data.posts ).length ) {
+		//			autocompleteResults = response.data.posts;
+		//
+		//			// Update the autocomplete list
+		//			awesome.list = Object.keys( response.data.posts );
+		//		}
+		//	} )
+		//	.always( function () {
+		//		processing = false;
+		//
+		//		// Check if the search was updated during processing
+		//		if ( search !== searchfield.val() ) {
+		//			searchfield.trigger( 'input.autocomplete' );
+		//		}
+		//	} );
+	}
+}
+
+init();
