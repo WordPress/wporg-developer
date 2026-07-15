cc106383a9a364a341ac7b9a1b5b3addd8f65345
Alex Kirk <akirk@users.noreply.github.com>
2026-07-15 16:07:11 +0200

    Render Playground Blueprint examples

--- source/wp-content/themes/wporg-developer-2023/inc/import-playground.php
+++ source/wp-content/themes/wporg-developer-2023/inc/import-playground.php
@@ -12,6 +12,7 @@ class DevHub_Playground_Importer extends DevHub_Docs_Importer {
 		);
 
 		add_filter( 'handbook_label', array( $this, 'change_handbook_label' ), 10, 2 );
+		add_filter( 'wporg_markdown_before_transform', array( $this, 'transform_mdx' ), 10, 2 );
 		add_filter( 'wporg_markdown_after_transform', array( $this, 'parse_callout_markdown' ), 10, 2 );
 		add_filter( 'wporg_markdown_after_transform', array( $this, 'rewrite_root_relative_links' ), 20, 2 );
 	}
@@ -29,6 +30,65 @@ class DevHub_Playground_Importer extends DevHub_Docs_Importer {
 		}
 
 		return $label;
+	}
+
+	/**
+	 * Transforms supported MDX and removes its import declarations.
+	 *
+	 * @param string $markdown  The Markdown before it is transformed to HTML.
+	 * @param string $post_type The post type being imported.
+	 * @return string
+	 */
+	public function transform_mdx( $markdown, $post_type ) {
+		if ( $this->get_post_type() !== $post_type ) {
+			return $markdown;
+		}
+
+		$markdown = preg_replace( '/^\s*import\s+.+?\s+from\s+([\'\"]).+?\1;\s*$/m', '', $markdown );
+
+		return preg_replace_callback(
+			'/<BlueprintExample\b(.*?)^\s*\/\s*>/ms',
+			array( $this, 'transform_blueprint_example' ),
+			$markdown
+		);
+	}
+
+	/**
+	 * Transforms a BlueprintExample MDX component into Markdown.
+	 *
+	 * @param array $matches The matched BlueprintExample component.
+	 * @return string
+	 */
+	public function transform_blueprint_example( $matches ) {
+		$attributes = $matches[1];
+		$blueprint  = '';
+		$display    = '';
+
+		if ( preg_match( '/\bdisplay=\{`(.*?)`\s*\}/s', $attributes, $display_match ) ) {
+			$display = trim( $display_match[1] );
+		}
+
+		if ( preg_match( '/\bblueprint=\{\{(.*)\}\}\s*$/s', $attributes, $blueprint_match ) ) {
+			$blueprint = '{' . trim( $blueprint_match[1] ) . '}';
+		}
+
+		$shown_blueprint = $display ?: $blueprint;
+		$parsed_blueprint = json_decode( $shown_blueprint, true );
+		if ( JSON_ERROR_NONE !== json_last_error() ) {
+			return $matches[0];
+		}
+
+		$output = array();
+		if ( ! preg_match( '/\bjustButton(?:=\{true\})?/', $attributes ) ) {
+			$output[] = "```json\n" . wp_json_encode( $parsed_blueprint, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES ) . "\n```";
+		}
+
+		if ( ! preg_match( '/\bnoButton(?:=\{true\})?/', $attributes ) ) {
+			$encoded_blueprint = base64_encode( wp_json_encode( $parsed_blueprint, JSON_UNESCAPED_SLASHES ) );
+			$output[] = '[Try it out!](https://playground.wordpress.net/?mode=seamless#' . $encoded_blueprint . ')';
+		}
+
+		return "\n\n" . implode( "\n\n", $output ) . "\n\n";
 	}
 
 	/**
