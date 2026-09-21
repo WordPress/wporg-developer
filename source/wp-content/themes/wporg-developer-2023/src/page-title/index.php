011d6b8ef9067baea0c6e7a789e35ae7275b14f7
Jon Surrell <sirreal@users.noreply.github.com>
2026-09-21 07:34:47 +0000

    Fix incorrect section close tag in page-title block

--- source/wp-content/themes/wporg-developer-2023/src/page-title/index.php
+++ source/wp-content/themes/wporg-developer-2023/src/page-title/index.php
@@ -42,7 +42,7 @@ function render( $attributes, $content, $block ) {
 
 	$wrapper_attributes = get_block_wrapper_attributes();
 	return sprintf(
-		'<%1$s %2$s>%3$s</section>',
+		'<%1$s %2$s>%3$s</%1$s>',
 		$tag_name,
 		$wrapper_attributes,
 		$title,
